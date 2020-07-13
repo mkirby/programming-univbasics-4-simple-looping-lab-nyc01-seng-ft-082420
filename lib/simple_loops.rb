b70f64c314b3bb590ee5ee12445fdc66a1b683b3
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-13 15:07:43 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,11 @@
 # Write your methods here
 def loop_message_five_times(string)
-  5.times do
+  #5.times do
+    #puts string
+  #end
+  
+  counter = 0
+  while counter <= 5 do
     puts string
   end
 end
